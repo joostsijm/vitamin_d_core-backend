5b1a8b189dc977a5274223db93ceeb9566ab97b0
JoostSijm <joostsijm@gmail.com>
2021-05-20 17:43:45 +0200

    Working on user resource

--- vitamin_d_resource_user/index.js
+++ vitamin_d_resource_user/index.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
@@ -16,20 +14,21 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-const Item = require('./models/Item');
+const User= require('./models/User');
 
 app.get('/', (req, res) => {
-  Item.find()
-    .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
+    res.setHeader('Content-Type', 'application/json');
+    User.find()
+        .then(users => res.end(JSON.stringify( users )))
+        .catch(err => res.status(404).json({ msg: 'No users found' }));
 });
 
-app.post('/item/add', (req, res) => {
-  const newItem = new Item({
+app.post('/user/add', (req, res) => {
+  const newUser = new User({
     name: req.body.name
   });
 
-  newItem.save().then(item => res.redirect('/'));
+  newUser.save().then(user => res.redirect('/'));
 });
 
 const port = 3000;
