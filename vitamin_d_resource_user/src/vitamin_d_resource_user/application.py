fb5ccb67d3593a39da0e46398f1c288c3669f3cb
JoostSijm <joostsijm@gmail.com>
2021-06-10 16:58:12 +0200

    Improving resource user

--- vitamin_d_resource_user/src/vitamin_d_resource_user/application.py
+++ vitamin_d_resource_user/src/vitamin_d_resource_user/application.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 
-from flask import Blueprint, abort, request, redirect, url_for, jsonify
+from flask import Blueprint, abort, request, jsonify, Response
 
 from vitamin_d_resource_user.models import User, NaamgegevensUser, \
     GeslachtsnaamUser, ContactgegevensUser, EmailAdressenUser, \
@@ -11,92 +11,83 @@ from vitamin_d_resource_user.models import User, NaamgegevensUser, \
 
 blueprint = Blueprint('application', __name__)
 
-@blueprint.route('/n')
-def home():
-    """Home route"""
-    return "hallo"
-
-
-@blueprint.route('/getdata/<username>')
+@blueprint.route('/<username>', methods=['GET'])
 def getdata(username):
-    """Get user data"""
+    """Get user"""
     if User.objects(User__naamgegevens__contactgegevens__emailAdressen__emailAdres=username):
-        get_data = User.objects(
+        user = User.objects(
                 User__naamgegevens__contactgegevens__emailAdressen__emailAdres=username
             )
     else:
-        get_data = Administrator.objects(
+        user = Administrator.objects(
                 Administrator__naamgegevens__contactgegevens__emailAdressen__emailAdres=username
             )
-    return jsonify(get_data)
+    return jsonify(user)
 
 
-@blueprint.route('/user', methods=['POST', 'GET'])
-def user_data():
-    """Manipulate user data"""
-    if request.method == 'POST':
-        geslacht = request.form['geslacht']
-        voornaam = request.form['voornaam']
-        achternaam = request.form['achternaam']
-        username = request.form['username']
-        password = request.form['password']
-        geboortedatum = request.form['geboortedatum']
-        lengte = request.form['lengte']
-        lengtedatum = datetime.now()
-        lengtepositie = request.form['lengtepositie']
-        gewicht = request.form['gewicht']
-        gewichtdatum = datetime.now()
-        gewichtpositie = request.form['gewichtpositie']
+@blueprint.route('/', methods=['POST'])
+def post_user():
+    """Post user"""
+    geslacht = request.form['geslacht']
+    voornaam = request.form['voornaam']
+    achternaam = request.form['achternaam']
+    username = request.form['username']
+    password = request.form['password']
+    geboortedatum = request.form['geboortedatum']
+    lengte = request.form['lengte']
+    lengtedatum = datetime.now()
+    lengtepositie = request.form['lengtepositie']
+    gewicht = request.form['gewicht']
+    gewichtdatum = datetime.now()
+    gewichtpositie = request.form['gewichtpositie']
 
-        user = User(geslacht=geslacht, geboortedatum=geboortedatum)
-        user.naamgegevens = NaamgegevensUser(voornamen=voornaam)
-        user.naamgegevens.geslachtsnaam = GeslachtsnaamUser(achternaam=achternaam)
-        user.naamgegevens.contactgegevens = ContactgegevensUser()
-        user.naamgegevens.contactgegevens.emailAdressen = EmailAdressenUser(emailAdres=username)
-        user.userdata.lichaamslengte = Lichaamslengte(
-                lengteWaarde=lengte,
-                lengteDatum=lengtedatum,
-                positie=lengtepositie
-            )
-        user.userdata.lichaamsgewicht = Lichaamsgewicht(
-                gewichtWaarde=gewicht,
-                gewichtDatum=gewichtdatum,
-                positie=gewichtpositie
-            )
-        user.save()
-        login = Login(username=username, password=password)
-        login.save()
-        return 'send'
-    return redirect(url_for('getdata', username=request.form['username']))
+    user = User(geslacht=geslacht, geboortedatum=geboortedatum)
+    user.naamgegevens = NaamgegevensUser(voornamen=voornaam)
+    user.naamgegevens.geslachtsnaam = GeslachtsnaamUser(achternaam=achternaam)
+    user.naamgegevens.contactgegevens = ContactgegevensUser()
+    user.naamgegevens.contactgegevens.emailAdressen = EmailAdressenUser(emailAdres=username)
+    user.userdata.lichaamslengte = Lichaamslengte(
+            lengteWaarde=lengte,
+            lengteDatum=lengtedatum,
+            positie=lengtepositie
+        )
+    user.userdata.lichaamsgewicht = Lichaamsgewicht(
+            gewichtWaarde=gewicht,
+            gewichtDatum=gewichtdatum,
+            positie=gewichtpositie
+        )
+    user.save()
+    login = Login(username=username, password=password)
+    login.save()
+    return Response(status=200)
 
-@blueprint.route('/admin', methods=['POST', 'GET'])
-def admin_data():
+
+@blueprint.route('/admin', methods=['POST'])
+def admin_post():
     """Manipulate admin data"""
-    if request.method == 'POST':
-        geslacht = request.form['geslacht']
-        voornaam = request.form['voornaam']
-        achternaam = request.form['achternaam']
-        username = request.form['username']
-        password = request.form['password']
-        geboortedatum = request.form['geboortedatum']
-        specialisme = request.form['specialisme']
+    geslacht = request.form['geslacht']
+    voornaam = request.form['voornaam']
+    achternaam = request.form['achternaam']
+    username = request.form['username']
+    password = request.form['password']
+    geboortedatum = request.form['geboortedatum']
+    specialisme = request.form['specialisme']
 
-        administrator = Administrator(
-                geslacht=geslacht,
-                geboortedatum=geboortedatum,
-                specialisme=specialisme
-            )
-        administrator.naamgegevens = NaamgegevensUser(voornamen=voornaam)
-        administrator.naamgegevens.geslachtsnaam = \
-                GeslachtsnaamUser(achternaam=achternaam)
-        administrator.naamgegevens.contactgegevens = ContactgegevensUser()
-        administrator.naamgegevens.contactgegevens.emailAdressen = \
-                EmailAdressenUser(emailAdres=username)
-        administrator.save()
-        login = Login(username=username, password=password)
-        login.save()
-        return 'send'
-    return redirect(url_for('getdata', username=request.form['username']))
+    administrator = Administrator(
+            geslacht=geslacht,
+            geboortedatum=geboortedatum,
+            specialisme=specialisme
+        )
+    administrator.naamgegevens = NaamgegevensUser(voornamen=voornaam)
+    administrator.naamgegevens.geslachtsnaam = \
+            GeslachtsnaamUser(achternaam=achternaam)
+    administrator.naamgegevens.contactgegevens = ContactgegevensUser()
+    administrator.naamgegevens.contactgegevens.emailAdressen = \
+            EmailAdressenUser(emailAdres=username)
+    administrator.save()
+    login = Login(username=username, password=password)
+    login.save()
+    return Response(status=200)
 
 
 @blueprint.errorhandler(404)
