3fe1e807fb89bafa458d193a92afaab440f18ea6
Jesse Jones <jesse.jones@hva.nl>
2021-06-15 13:46:28 +0000

    Update vitamin_d_fitbit/src/vitamin_d_fitbit/application.py

--- vitamin_d_fitbit/src/vitamin_d_fitbit/application.py
+++ vitamin_d_fitbit/src/vitamin_d_fitbit/application.py
@@ -12,6 +12,34 @@ def index():
     """Get data from fitbit"""
     return 'send'
 
+
+@blueprint.route('/afstand/username/activiteit/datum', methods=['POST'])
+def afstand(username, activiteit, datum):
+    """Post of de activiteit is behaald"""
+    if activiteit == lopen:
+        afstandactiviteit = distancewalked()
+    elif activiteit == rennen:
+        afstandactiviteit = distanceran()
+    elif activiteit == fietsen:
+        afstandactiviteit = distancebiked()
+    else: 
+        afstandactiviteit = distanceswam()
+
+    qset = Schedule.objects(username=username, activiteit=activiteit, activiteitDatum=datum)
+    doc = qset.first()
+    if int(doc.geplandeafstand) <= afstandactiviteit:
+        return True
+    else:
+        return False
+
+
+# totaldistance()
+# distancewalked()
+# distanceran()
+# distancebiked()
+# distanceswam()
+
+
 @blueprint.errorhandler(404)
 def page_not_found(error):
     """Handle 404 errors"""
